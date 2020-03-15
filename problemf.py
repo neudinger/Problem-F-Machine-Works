7134eb5709535b1ee69540edaf5f2a300ec74e91
Kevin Barré <kevin.barre@epitech.eu>
2020-03-15 16:19:23 +0100

    (dev)[fix]: dont add if buy branche doesnt exist, optimisation Changes to be committed: 	modified:   problemf.py 	new file:   snapshot_input.txt 	new file:   works.ans

--- problemf.py
+++ problemf.py
@@ -12,69 +12,62 @@ import sys
 # https://fr.wikipedia.org/wiki/21_probl%C3%A8mes_NP-complets_de_Karp
 
 
-def read_file(filename)->list:
+def read_file(filename) -> list:
     with open(filename, 'r') as f:
         cases = list()
         company_info = f.readline().strip().split(' ')
         while company_info != ['0']*3:
             # initialize a case
-            transaction_days ,wallet, trade_days = tuple(map(lambda x: int(x), company_info))
+            transaction_days, wallet, trade_days = tuple(
+                map(lambda x: int(x), company_info))
             transactions = list()
             # add list of machine info
             for i in range(transaction_days):
-                transactions.append(tuple(map(lambda x: int(x), f.readline().strip().split(' '))))
+                transactions.append(
+                    tuple(map(lambda x: int(x), f.readline().strip().split(' '))))
             # after all machines read, add this to the case
-            cases.append([trade_days, (wallet, 0, 0), transactions])
+            cases.append([trade_days,
+                          (wallet, 0, 0), transactions])
             company_info = f.readline().strip().split(' ')
     return cases
 
-def compute(nodes:list, days:int=1):
+
+def compute(nodes: list, days: int = 1):
     # nodes
     # current_money, resale_value,
     for index, node in enumerate(nodes):
         nodes[index] = (node[0] + node[2]*days, node[1], node[2])
 
-def choose_buy(node:tuple, transaction:tuple):
-    # nodes
-    # current_money, resale_value, computeprofit
-    # if you have enough money buy
-    transaction_day, price, resale_value, computeprofit = transaction
-    if node[0]+node[1] >= price:
-        return (node[0]+node[1]-price, resale_value, computeprofit)
-    # Else compute to get money
-    return (node[0]+node[2], node[1], node[2])
 
-def apply_transaction(transaction:tuple=(), branches:list=[])->list:
+def apply_transaction(transaction: tuple = (), branches: list = []) -> list:
     next_branch = list()
     for branche in branches:
-        buy = choose_buy(node=branche, transaction=transaction)
-        # buy and compute
-        if buy:
-            next_branch.append(buy)
-        # and stay
-        next_branch.append(branche)
+        transaction_day, price, resale_value, computeprofit = transaction
+        # Buy if possible
+        buy_branche = (branche[0]+branche[1]-price, resale_value, computeprofit) if branche[0] + \
+            branche[1] >= price else None
+        if buy_branche:
+            next_branch.append(buy_branche)
+
+        # Stay Branche
+        next_branch.append((branche[0]+branche[2], branche[1], branche[2]))
     return next_branch
 
-def bestroad(transactions:list=[])->int:
+
+def bestroad(transactions: list = []) -> int:
     trade_days = transactions.pop(0)
     branches = [transactions.pop(0)]
-    last_day = 0
-    for idx, transaction in enumerate(sorted(transactions.pop(0))):
-        # transaction_day, price, resale_value, computeprofit
-        
-        # If you buy a machine M i on day D i , then ACM can operate it starting on day D i + 1. Each day that the machine
-        # operates, it produces a profit of G i dollars for the company.
-        # Jump the first day
-        if transaction[0] - last_day > 1 and idx:
-            # You cannot operate a machine on the day that you sell
-            # it, but you may sell a machine and use the proceeds to buy a new machine on the same day.
-            compute(branches, transaction[0]-last_day-1)
+    last_day = 1
+    # last_transaction = None
+    for idx, transaction in enumerate(sorted(transactions.pop(0)), 1):
+        compute(branches, transaction[0] - last_day-1)
         branches = apply_transaction(transaction, branches)
         last_day = transaction[0]
-    # compute until the end
+
     compute(branches, trade_days - last_day)
     # sort by money and sell everythings
     return list(sorted(map(lambda x: (x[0] + x[1]), branches)))[-1]
+
 
 if __name__ == '__main__':
     transactions = read_file(sys.argv[1])
